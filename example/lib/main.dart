25aa22a549ddd774757e4da06a1c9807114bd891
Kenneth Zeng <kkzeng@edu.uwaterloo.ca>
2020-08-19 18:12:53 +0800

    Modified app to display the generation results

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,10 +11,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String keyHandle;
-  String clientData;
-  String attestationObj;
-  String signature;
+  String keyHandle; // Should be saved in shared pref
+  static const String regChallenge = "randomchallenge1231321"; // Should come from server
+  static const String signChallenge = "transaction12356678"; // Should come from server
+
+  String displayText = "Please sign or register to display auth results";
   static final String rpDomain = "mojapay-test-rp.web.app";
   static final String rpName = "MojapayFido2Demo";
   Fido2Client fidoClient = Fido2Client();
@@ -27,44 +28,54 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child:
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-            buildRegButton(),
-            buildSignButton(),
-          ],)
+              Text(displayText),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  buildRegButton(),
+                  buildSignButton(),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget buildSignButton() {
-    return RaisedButton(child: Text('FIDO Sign'), onPressed: () {
-      SigningResultListener signingListener = (String keyHandle, String clientData, String authData, String signature, String userHandle) {
-        this.signature = signature;
-        print('Signature: $signature');
-        print('Key Handle: $keyHandle');
-        print('User handle: $userHandle');
-      };
-      fidoClient.addSigningResultListener(signingListener);
-      fidoClient.initiateSigningProcess(keyHandle, "transaction12356678", rpDomain);
-    },);
+  Widget buildDisplayText() {
+    return Text(displayText);
   }
 
   Widget buildRegButton() {
     return RaisedButton(child: Text('FIDO Register'), onPressed: () {
       RegistrationResultListener regListener = (String keyHandle, String clientData, String attestationObj) {
-        this.keyHandle = keyHandle;
-        this.clientData = clientData;
-        this.attestationObj = attestationObj;
-        print('Key handle: $keyHandle');
-        print('Client data: $clientData');
-        print('Attestation obj: $attestationObj');
+        setState(() {
+          this.keyHandle = keyHandle;
+          displayText = 'Challenge: \n$regChallenge\n\nKey handle: \n$keyHandle\n\nClient data: \n$clientData\n\nAttestation obj: \n$attestationObj';
+        });
+        print(displayText);
       };
       fidoClient.addRegistrationResultListener(regListener);
-      fidoClient.initiateRegistrationProcess("randomchallenge1231321", "kenkaizeng@gmail.com", "kkzeng", rpDomain, rpName);
+      fidoClient.initiateRegistrationProcess(regChallenge, "kenkaizeng@gmail.com", "kkzeng", rpDomain, rpName);
+    },);
+  }
+
+  Widget buildSignButton() {
+    return RaisedButton(child: Text('FIDO Sign'), onPressed:  keyHandle == null ? null : () {
+      SigningResultListener signingListener = (String keyHandle, String clientData, String authData, String signature, String userHandle) {
+        setState(() {
+          displayText = 'Challenge: \n$signChallenge\n\nKey handle: \n$keyHandle\n\nClient data: \n$clientData\n\nauthData: \n$authData\n\nSignature: \n$signature';
+        });
+        print(displayText);
+      };
+      fidoClient.addSigningResultListener(signingListener);
+      fidoClient.initiateSigningProcess(keyHandle, signChallenge, rpDomain);
     },);
   }
 }
